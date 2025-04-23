0aeb1a1da6a171c82f0ac124ebf04da587df78a2
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-04-23 12:14:09 +0200

    PestAndPint

---
+++ tests/Pest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Pest\Laravel;
+
+beforeEach(function () {
+    // Any setup code can go here
+});
+
+afterEach(function () {
+    // Any teardown code can go here
+});
+
+// Add additional global helpers or hooks for your tests if needed
