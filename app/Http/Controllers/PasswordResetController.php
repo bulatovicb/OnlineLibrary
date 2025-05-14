a50b87a2c4322c74656b59e128b1aabf46e2950b
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-05-14 13:25:20 +0200

    Reset your password
    https://matijabojanic.atlassian.net/browse/OL3-57

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Role;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\Rule;
 
@@ -48,4 +51,52 @@ class PasswordResetController extends Controller
             ], 400);
         }
     }
+
+    /**
+     * Resets password using received token.
+     *
+     * Validates the received token.
+     * Validates that password and confirmed password fields match.
+     * Log in the librarian.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => [
+                'required',
+                'email',
+                Rule::exists('users', 'email')->where(function ($query) {
+                    $query->where('role_id', Role::LIBRARIAN);
+                }),
+            ],
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) use ($request) {
+                $user->password = Hash::make($password);
+                $user->save();
+                event(new PasswordReset($user));
+
+                Auth::login($user);
+            }
+        );
+
+        if ($status === Password::PASSWORD_RESET) {
+            return response()->json([
+                'message' => __($status)
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => __($status)
+            ], 400);
+        }
+
+    }
+
+
 }
