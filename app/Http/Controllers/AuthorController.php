abcb1271feec59e2aea8277b9b9f78575a489211
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-05-16 11:15:08 +0200

    Create author
    https://matijabojanic.atlassian.net/browse/OL3-29

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthorController extends Controller
+{
+    /**
+     * Creates new author.
+     *
+     * Accessible only by authenticated librarians.
+     * Validates the provided profile data and creates a new user if validation passes.
+     * Returns a JSON response with the author data and a success message.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+
+        if (!Auth::check() || !Auth::user()->isLibrarian()) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'biography' => 'nullable|string',
+            'picture' => 'nullable|image|max:5120',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $picturePath = null;
+        if ($request->hasFile('picture')) {
+            $picturePath = $request->file('picture')->store('picture', 'public');
+        }
+
+        $author = Author::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'biography' => $request->biography,
+            'picture' => $picturePath,
+        ]);
+
+        return response()->json([
+            'message' => 'Author created successfully',
+            'author' => $author
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PasswordResetController;
+use App\Http\Controllers\AuthorController;
 
-Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('logout', [AuthController::class, 'logout']);
@@ -19,6 +20,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::get('users/{user}', [UserController::class, 'show']);
         Route::get('/users/{user}/profile-picture', [UserController::class, 'profilePicture'])->name('user.profilePicture');
         Route::delete('users/{user}', [UserController::class, 'destroy']);
+        Route::post('/authors', [AuthorController::class, 'create']);
     });
 });
 Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLinkEmail']);
