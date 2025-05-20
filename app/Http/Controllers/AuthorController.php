5dfa3bef9ac6e2298d4f99c456e23ea03dec0db7
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-05-20 10:41:15 +0200

    Change string to text, remove auth check
    https://matijabojanic.atlassian.net/browse/OL3-29

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 
@@ -23,9 +22,6 @@ class AuthorController extends Controller
     public function create(Request $request)
     {
 
-        if (!Auth::check() || !Auth::user()->isLibrarian()) {
-            return response()->json(['error' => 'Unauthorized'], 403);
-        }
 
         $validator = Validator::make($request->all(), [
             'first_name' => 'required|string',
