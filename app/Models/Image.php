f93bceb2ce66ded31f06d55aa4ff969873b05e98
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-05-23 09:25:06 +0200

    Database structure for books
    https://matijabojanic.atlassian.net/browse/OL3-17

---
+++ app/Models/Image.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'path',
+        'type'
+    ];
+
+    /**
+     * Gets the book associated with this image.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+}
