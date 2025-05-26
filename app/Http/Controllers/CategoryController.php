ad8e1536616acf5c6e9ab5261b93cca9d3c9bfe6
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-05-26 11:49:43 +0200

    Create category
    https://matijabojanic.atlassian.net/browse/OL3-66

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Creates new category for books.
+     *
+     * Validates the provided category's data and creates a new category if validation passes.
+     * Handles icon uploads if any and store the icon.
+     * Returns a JSON response with created category.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'icon' => 'nullable|image|max:5120',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        
+        $iconPath = null;
+
+        if ($request->hasFile('icon')) {
+            $iconPath = $request->file('icon')->store('icons', 'public');
+        }
+
+        $category = Category::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'icon' => $iconPath,
+        ]);
+
+        return response()->json([
+            'message' => 'Category created successfully',
+            'category' => $category
+
+        ], 201);
+
+
+    }
 }
