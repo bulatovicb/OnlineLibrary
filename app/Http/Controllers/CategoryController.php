d1e24679b5d55e399dba390f13ba841fa328ea99
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-05-26 12:25:43 +0200

    Category view
    https://matijabojanic.atlassian.net/browse/OL3-67

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Shows a category.
+     *
+     * Accessible only by authenticated librarians.
+     * Returns a JSON response with category data.
+     * Automatically returns 404 if the category is not found.
+     *
+     * @param Category $category
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Category $category)
+    {
+        return response()->json([
+            'category' => $category,
+        ], 200);
+    }
+
+    /**
+     * Shows a category icon.
+     *
+     * Accessible only by authenticated librarians.
+     * Returns JSON error response if the category icon is not found.
+     * Otherwise, returns the icon path.
+     *
+     * @param Category $category
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function categoryIcon(Category $category)
+    {
+        if (!$category->icon) {
+            return response()->json([
+                'message' => 'Category icon not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'icon_url' => $category->icon,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PasswordResetController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -27,6 +28,9 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::patch('/authors/{author}/update', [AuthorController::class, 'update']);
         Route::post('/authors/{author}/update-picture', [AuthorController::class, 'updatePicture']);
         Route::delete('/authors/{author}/destroy', [AuthorController::class, 'destroy']);
+
+        Route::get('/categories/{category}', [CategoryController::class, 'show']);
+        Route::get('/categories/{category}/icon', [CategoryController::class, 'categoryIcon'])->name('category.categoryIcon');
     });
 });
 Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLinkEmail']);
