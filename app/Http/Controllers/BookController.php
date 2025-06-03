f0b3cc3098ec2b12039e0d5a7bb842546120c611
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-06-03 09:54:57 +0200

    Create CreateBookRequest
    https://matijabojanic.atlassian.net/browse/OL3-3

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,52 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateBookRequest;
 use App\Models\Book;
-use App\Models\Image;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 
 class BookController extends Controller
 {
     /**
-     * Creates new book.
+     *  Creates new book.
      *
-     * Accessible only by authenticated librarians.
-     * Validates the provided book's data and creates a new book if validation passes.
-     * Handles image uploads if any and store the image.
-     * Attach related models and eager load related data before returning response.
-     * Returns a JSON response with created book and its relations.
+     *  Accessible only by authenticated librarians.
+     *  Validates the provided book's data via CreateBookRequest.
+     *  Handles image uploads if any and store the image.
+     *  Attaches related models and eager load related data before returning response.
+     *  Returns a JSON response with created book and its relations.
      *
-     * @param Request $request
+     * @param CreateBookRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function create(Request $request)
+    public function create(CreateBookRequest $request)
     {
-        $validator = Validator::make(request()->all(), array_merge([
-            'name' => 'required',
-            'description' => 'required',
-            'number_of_pages' => 'required',
-            'number_of_copies_available' => 'required',
-            'isbn' => 'required',
-            'language' => 'nullable',
-            'script' => ['nullable', Rule::in(Book::SCRIPTS)],
-            'binding' => ['nullable', Rule::in(Book::BINDINGS)],
-            'dimensions' => ['nullable', Rule::in(Book::DIMENSIONS)],
-            'categories' => 'nullable|array',
-            'categories.*' => 'exists:categories,id',
-            'genres' => 'nullable|array',
-            'genres.*' => 'exists:genres,id',
-            'authors' => 'required|array',
-            'authors.*' => 'exists:authors,id',
-            'publishers' => 'nullable|array',
-            'publishers.*' => 'exists:publishers,id',
-        ], Image::validationRules()));
-
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 422);
-        }
-
         $book = Book::create($request->only([
             'name',
             'description',
