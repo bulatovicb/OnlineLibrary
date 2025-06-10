acb364942fac33d4923a03ca2b63bb7f5213ce5e
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-06-10 08:42:49 +0200

    Add parameters in create function

--- app/Jobs/ImportBooksJob.php
+++ app/Jobs/ImportBooksJob.php
@@ -9,20 +9,20 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\RateLimiter;
 
 class ImportBooksJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $query;
-
+    protected $copiesAvailable;
     /**
      * Create a new job instance.
      */
-    public function __construct($query)
+    public function __construct($query, $copiesAvailable)
     {
         $this->query = $query;
+        $this->copiesAvailable = $copiesAvailable;
     }
 
     /**
@@ -30,25 +30,27 @@ class ImportBooksJob implements ShouldQueue
      */
     public function handle(): void
     {
-        RateLimiter::attempt('google-books-api', function () {
+        $response = Http::get('https://www.googleapis.com/books/v1/volumes', [
+            'q' => $this->query,
+        ]);
 
-            $response = Http::get('https://www.googleapis.com/books/v1/volumes', [
-                'q' => $this->query,
-            ]);
-
-            if ($response->successful()) {
-                $books = $response->json()['items'] ?? [];
-                foreach ($books as $bookData) {
-                    $volumeInfo = $bookData['volumeInfo'];
-                    Book::create([
-                        'name' => $volumeInfo['title'] ?? 'No title',
-                        'description' => $volumeInfo['description'] ?? 'No description',
-                        'number_of_pages' => $volumeInfo['pageCount'] ?? 0,
-
-                    ]);
-                }
+        if ($response->successful()) {
+            $books = $response->json()['items'] ?? [];
+            foreach ($books as $bookData) {
+                $volumeInfo = $bookData['volumeInfo'];
+                Book::create([
+                    'name' => $volumeInfo['title'] ?? 'No title',
+                    'description' => $volumeInfo['description'] ?? 'No description',
+                    'number_of_pages' => $volumeInfo['pageCount'] ?? 0,
+                    'number_of_copies_available' => $this->copiesAvailable ,
+                    'isbn' => $volumeInfo['industryIdentifiers'][0]['identifier'] ?? uniqid(),
+                    'language' => $volumeInfo['language'] ?? 'unknown',
+                    'script' => 'Latin',
+                    'binding' => 'Paperback',
+                    'dimensions' => 'N/A',
+                ]);
             }
-        }, 100);
+        }
 
     }
 }
