ee1155d330c34be8ab91179e5be8725b79776f35
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-06-12 14:46:07 +0200

    Remove validateUserRoles function

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -7,17 +7,5 @@ use App\Models\User;
 
 class RentalController extends Controller
 {
-    public function validateUserRoles(int $studentId, int $librarianId)
-    {
-        $student = User::findOrFail($studentId);
-        $librarian = User::findOrFail($librarianId);
-
-        if ($student->role_id !== Role::STUDENT) {
-            throw new \Exception("Selected user is not a student");
-        }
-
-        if ($librarian->role_id !== Role::LIBRARIAN){
-            throw new \Exception("Selected user is not a librarian");
-        }
-    }
+    
 }
--- database/migrations/2025_06_12_072401_create_rentals_table.php
+++ database/migrations/2025_06_12_072401_create_rentals_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('rentals', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('book_id')->constrained('books');
-            $table->foreignId('student_id')->constrained('users');
-            $table->foreignId('librarian_id')->constrained('users');
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('librarian_id')->constrained('users')->onDelete('cascade');
             $table->timestamp('rented_at');
             $table->timestamp('returned_at')->nullable()->default(null);
             $table->timestamps();
