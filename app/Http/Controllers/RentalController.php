bafe66e323d720a62058385c0c5cb0973ed65241
bulatovicb <bojana.bulatovic.obradovic@gmail.com>
2025-06-23 14:20:34 +0200

    View overdue book list
    https://matijabojanic.atlassian.net/browse/OL3-54

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -2,10 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Role;
-use App\Models\User;
+use App\Models\Policy;
+use App\Models\Rental;
+use Illuminate\Http\Request;
 
 class RentalController extends Controller
 {
-    
+    /**
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function indexOverdue(Request $request)
+    {
+        $request->validate([
+            'search_value' => 'nullable|string',
+            'per_page' => 'nullable|integer|in:20,50,100',
+        ]);
+
+        $rentalPolicy = Policy::where('name', 'rental_period')->first();
+        $rentalPeriod = $rentalPolicy->period;
+
+        $query = Rental::with([
+            'book',
+            'librarian',
+            'student'
+        ])->whereNull('returned_at')
+            ->whereDate('rented_at', '<=', now()->subDays($rentalPeriod));
+
+        if ($request->filled('search_value')) {
+            $search = $request->search_value;
+            $query->whereHas('book', function ($q) use ($search) {
+                $q->whereRaw("name ILIKE ?", ["%{$search}%"]);
+            });
+        }
+
+        $perPage = $request->per_page ?? 20;
+        $overdueRentals = $query->paginate($perPage);
+
+        $overdueRentals->getCollection()->transform(function ($rental) use ($rentalPeriod) {
+            $daysRented = now()->diffInDays($rental->rented_at);
+            $daysOverdue = $daysRented - $rentalPeriod;
+
+            return [
+                'book_title' => $rental->book->name,
+                'rental_date' => $rental->rented_at ? $rental->rented_at->toDateTimeString() : null,
+                'rented_by' => [
+                    'name' => $rental->student->first_name,
+                    'last_name' => $rental->student->last_name,
+                    'id' => $rental->student->id,
+                ],
+                'total_rental_days' => $daysRented,
+                'days_overdue' => $daysOverdue > 0 ? $daysOverdue : 0,
+            ];
+        });
+
+        return response()->json([
+            'message' => "Success",
+            'data' => $overdueRentals,
+        ]);
+
+    }
 }
--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -17,6 +17,11 @@ class Rental extends Model
         'returned_at',
     ];
 
+    protected $casts = [
+        'rented_at' => 'datetime',
+        'returned_at' => 'datetime',
+    ];
+
     public function book()
     {
         return $this->belongsTo(Book::class);
